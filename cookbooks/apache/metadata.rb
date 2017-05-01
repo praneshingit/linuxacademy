ba1951e74c7aaefeb00b09619864bd044275c9bf
root <root@praneshinlinuxacademy1.mylabserver.com>
2017-05-01 19:52:21 +0000

    added apache cookbook

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,5 +1,5 @@
 name 'apache'
-maintainer 'Pranesh'
+maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,10 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
 
 package 'apache2' do
 	package_name 'httpd'
-	action	:install
+	action :install
 end
 
 service 'apache2' do
